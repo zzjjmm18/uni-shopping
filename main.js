5e77be43bbc1fff39fdae8c4da14cff6d0050067
jm <1638718049@qq.com>
2022-10-08 22:20:09 +0800

    分类模块页面完成

--- main.js
+++ main.js
@@ -23,6 +23,15 @@ $http.afterRequest = function() {
   uni.hideLoading()
 }
 
+// 封装弹框的方法
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
