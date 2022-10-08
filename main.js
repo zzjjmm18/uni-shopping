7dfc6b9677da5e2616be602d28fb8443d90e8e00
jm <1638718049@qq.com>
2022-10-08 22:01:35 +0800

    首页模块完成

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ import { $http } from '@escook/request-miniprogram'
 uni.$http = $http
 
 // 请求的根路径
-$http.baseUrl = 'https://www.uinav.com'
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
 
 // 请求拦截器
 $http.beforeRequest = function(options) {
