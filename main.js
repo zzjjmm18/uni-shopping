5f32c2eb6f1edb1e642ce553909e6df746761a75
jm <1638718049@qq.com>
2022-10-08 21:19:26 +0800

    小程序初始化完成

---
+++ main.js
@@ -0,0 +1,45 @@
+
+// #ifndef VUE3
+import Vue from 'vue'
+import App from './App'
+
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+// 请求的根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+
+// 响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+import App from './App.vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
